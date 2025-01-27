df55422fcbf7e41ddd2959c76313b9a11a443378
EnzoPennisi <163669748+EnzoPennisi@users.noreply.github.com>
2025-01-27 15:01:14 -0300

    Add vercel.json to fix redirection rules

--- src/components/Header/LogginButton.tsx
+++ src/components/Header/LogginButton.tsx
@@ -1,11 +1,13 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 export function LogginButton() {
-    const { loginWithPopup } = useAuth0();
+    const { loginWithRedirect } = useAuth0();
 
     return (
         <button className="px-6 py-2 rounded-sm text-sm font-medium hover:text-fuchsia-600 transition-all cursor-pointer"
-            onClick={() => loginWithPopup()}
+            onClick={() => loginWithRedirect({
+                appState: { returnTo: "/dashboard" } // Redirige al usuario a la página actual después de iniciar sesión
+            })}
         >
             Iniciar Sesión
         </button>
--- src/components/Header/RegisterButton.tsx
+++ src/components/Header/RegisterButton.tsx
@@ -2,12 +2,13 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 export function RegisterButton() {
 
-    const { loginWithPopup } = useAuth0();
+    const { loginWithRedirect } = useAuth0();
 
     return (
         <button className="px-6 py-2 rounded-sm text-sm font-medium bg-[#3c213598] hover:bg-[#5a314f98] transition-all cursor-pointer"
-            onClick={() => loginWithPopup(
+            onClick={() => loginWithRedirect(
                 {
+                    appState: { returnTo: "/dashboard" }, // Retorna al usuario a la página actual después de iniciar sesión
                     authorizationParams: { screen_hint: "signup" } // Muestra la pantalla de registro
                 }
             )}
