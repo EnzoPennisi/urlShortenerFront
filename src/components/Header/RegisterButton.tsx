4961b3f13cf89e0160f335876dd1cee5296f1510
EnzoPennisi <163669748+EnzoPennisi@users.noreply.github.com>
2025-01-26 23:04:05 -0300

    change loginWithRedirect to loginWithPopup

--- src/components/Header/RegisterButton.tsx
+++ src/components/Header/RegisterButton.tsx
@@ -2,13 +2,12 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 export function RegisterButton() {
 
-    const { loginWithRedirect } = useAuth0();
+    const { loginWithPopup } = useAuth0();
 
     return (
         <button className="px-6 py-2 rounded-sm text-sm font-medium bg-[#3c213598] hover:bg-[#5a314f98] transition-all cursor-pointer"
-            onClick={() => loginWithRedirect(
+            onClick={() => loginWithPopup(
                 {
-                    appState: { returnTo: "/dashboard" }, // Retorna al usuario a la página actual después de iniciar sesión
                     authorizationParams: { screen_hint: "signup" } // Muestra la pantalla de registro
                 }
             )}
